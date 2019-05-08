21f1a1d4f09262de7985017c8a2d96c432a95938
langemand <henrik.scheel@spjeldager.dk>
2019-05-08 21:36:08 +0200

    Add OnBoardChargerCommand to read OBC data

---
+++ app/src/androidTest/java/org/hexpresso/soulevspy/ObcCommandTest.java
@@ -0,0 +1,51 @@
+package org.hexpresso.soulevspy;
+
+import android.test.AndroidTestCase;
+import android.util.Pair;
+
+import org.hexpresso.soulevspy.obd.commands.OnBoardChargerCommand;
+import org.hexpresso.soulevspy.obd.values.CurrentValuesSingleton;
+import org.hexpresso.soulevspy.util.ClientSharedPreferences;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ObcCommandTest extends AndroidTestCase {
+    final String msgOk = "OK \r" +
+            ">";
+
+    final String soulEv2015Obc2102 = "79C 10 17 61 02 E8 03 1F 00 \r" +
+            "79C 21 01 00 09 23 10 0C 12 \r" +
+            "79C 22 8F 0E E0 00 85 00 4A \r" +
+            "79C 23 18 12 1C 00 00 00 00\r" +
+            ">";
+
+    public void testSoulVmcuCommand() {
+        CurrentValuesSingleton vals = CurrentValuesSingleton.reset();
+        ClientSharedPreferences prefs = new ClientSharedPreferences(this.getContext());
+        vals.setPreferences(prefs);
+
+        List<Pair<String, String>> reqres = Arrays.asList(
+                new Pair<String, String>("AT SH 7DF", msgOk),
+                new Pair<String, String>("AT CRA 79C", msgOk),
+                new Pair<String, String>("21 02", soulEv2015Obc2102)
+        );
+        Responder responder = new Responder(reqres);
+
+        OnBoardChargerCommand cmd = new OnBoardChargerCommand();
+        try {
+            cmd.execute(responder.getInput(), responder.getOutput());
+            cmd.doProcessResponse();
+        } catch (Exception e) {
+            assertEquals("", e.getMessage());
+        }
+
+        assertEquals(233.9, vals.get(R.string.col_obc_ac_in_V));
+        assertEquals(380.8, vals.get(R.string.col_obc_dc_out_V));
+        assertEquals(13.3, vals.get(R.string.col_obc_ac_in_A));
+        assertEquals(24.666666666666668, vals.get(R.string.col_obc_pilot_duty_cycle));
+        assertEquals(24, vals.get(R.string.col_obc_temp_1_C));
+        assertEquals(18, vals.get(R.string.col_obc_temp_2_C));
+        assertEquals(28, vals.get(R.string.col_obc_temp_3_C));
+    }
+}
---
+++ app/src/main/java/org/hexpresso/soulevspy/obd/commands/OnBoardChargerCommand.java
@@ -0,0 +1,73 @@
+package org.hexpresso.soulevspy.obd.commands;
+
+import org.hexpresso.elm327.commands.AbstractMultiCommand;
+import org.hexpresso.elm327.commands.filters.RegularExpressionResponseFilter;
+import org.hexpresso.obd.ObdMessageData;
+import org.hexpresso.soulevspy.R;
+import org.hexpresso.soulevspy.obd.values.CurrentValuesSingleton;
+
+import java.util.List;
+
+/**
+ * Created by henrik on 08/05/2019.
+ */
+
+public class OnBoardChargerCommand extends AbstractMultiCommand {
+    private BasicCommand mCmd2102 = null;
+
+    public OnBoardChargerCommand() {
+        addCommand(new BasicCommand("AT SH 7DF")); //"AT SH 7C5"));
+        addCommand(new BasicCommand("AT CRA 79C"));
+        mCmd2102 = new BasicCommand("21 02");
+        addCommand(mCmd2102);
+
+        mCmd2102.addResponseFilter(new RegularExpressionResponseFilter("^79C(.*)$"));
+//        mCmd2101.addResponseFilter(new RemoveSpacesResponseFilter());
+    }
+
+    public void doProcessResponse() {
+        CurrentValuesSingleton vals = CurrentValuesSingleton.getInstance();
+        try {
+            mCmd2102.getResponse().process();
+            List<String> lines = mCmd2102.getResponse().getLines();
+            if (lines.size() != 4)
+                return;
+
+            ObdMessageData obdData1 = new ObdMessageData(lines.get(1));
+
+            double acInV = (obdData1.getDataByte(3)*256+obdData1.getDataByte(4)) / 10.0;
+            vals.set(R.string.col_obc_ac_in_V, acInV);
+
+            ObdMessageData obdData2 = new ObdMessageData(lines.get(2));
+
+            boolean isEarlyVersion = (obdData2.getDataByte(4) == 0 && obdData2.getDataByte(5) == 0);
+            double dcOutV;
+            double acInA;
+            if (isEarlyVersion) {
+                dcOutV = (obdData1.getDataByte(7)*256 + obdData2.getDataByte(1)) / 10.0;
+                acInA = (obdData2.getDataByte(2)*256 + obdData2.getDataByte(3)) / 10.0;
+            } else {
+                dcOutV = (obdData2.getDataByte(2)*256 + obdData2.getDataByte(3)) / 10.0;
+                acInA = (obdData2.getDataByte(4)*256 + obdData2.getDataByte(5)) / 10.0;
+            }
+            vals.set(R.string.col_obc_dc_out_V, dcOutV);
+            vals.set(R.string.col_obc_ac_in_A, acInA);
+
+            int duty_cycle = obdData2.getDataByte(7);
+            vals.set(R.string.col_obc_pilot_duty_cycle, Double.valueOf(duty_cycle/3.0));
+
+            ObdMessageData obdData3 = new ObdMessageData(lines.get(3));
+
+            int temp1 = obdData3.getDataByte(1);
+            vals.set(R.string.col_obc_temp_1_C, temp1);
+
+            int temp2 = obdData3.getDataByte(2);
+            vals.set(R.string.col_obc_temp_2_C, temp2);
+
+            int temp3 = obdData3.getDataByte(3);
+            vals.set(R.string.col_obc_temp_3_C, temp3);
+        } catch (Exception e) {
+            //
+        }
+    }
+}
