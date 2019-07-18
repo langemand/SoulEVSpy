d092844ec86addfcecabb8c3bc540d7c55c22b0e
langemand <henrik.scheel@spjeldager.dk>
2019-07-18 17:50:47 +0200

    Add testcase  for SOC with one decimal, PID 594

--- app/src/androidTest/java/com/evranger/soulevspy/obd/StateOfChargeWithOneDecimalMessageFilterTest.java
+++ app/src/androidTest/java/com/evranger/soulevspy/obd/StateOfChargeWithOneDecimalMessageFilterTest.java
@@ -1,41 +1,62 @@
 package com.evranger.soulevspy.obd;
 
 import android.test.AndroidTestCase;
+import android.util.Pair;
 
-import com.evranger.obd.ObdMessageData;
+import com.evranger.elm327.commands.AbstractCommand;
+import com.evranger.elm327.exceptions.DataErrorException;
+import com.evranger.soulevspy.R;
+import com.evranger.soulevspy.Responder;
+import com.evranger.soulevspy.obd.commands.FilteredMonitorCommand;
+import com.evranger.soulevspy.obd.values.CurrentValuesSingleton;
+import com.evranger.soulevspy.util.ClientSharedPreferences;
 
 import junit.framework.Assert;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 /**
- * Created by Tyrel on 10/17/2015.
+ * Created by Henrik on 2019-07-18.
  */
 public class StateOfChargeWithOneDecimalMessageFilterTest extends AndroidTestCase {
+    final String msgOk = "OK \r" +
+            ">";
 
-    public void testProcessesZero() {
-        SpeedPreciseMessageFilter filter = new SpeedPreciseMessageFilter();
-        ObdMessageData messageData = new ObdMessageData("4F2 01 00 00 10 00 00 80 00");
-        filter.doProcessMessage(messageData);
-        Assert.assertEquals(0.0, filter.getSpeedInKmH());
+    final String msg594 = "594 54 17 28 23 C4 BE 01 00 <DATA ERROR \r" +
+            ">";
+
+    public void testProcessesOdoAndSpeedUsingResponder() throws IOException {
+        CurrentValuesSingleton vals = CurrentValuesSingleton.reset();
+        ClientSharedPreferences prefs = new ClientSharedPreferences(this.getContext());
+        vals.setPreferences(prefs);
+
+        List<Pair<String, String>> reqres = Arrays.asList(
+                new Pair<String, String>("AT CRA 594", msgOk),
+                new Pair<String, String>("AT MA", msg594),
+                new Pair<String, String>(".", "STOPPED\r>"),
+                new Pair<String, String>("AT AR", msgOk)
+        );
+
+        Responder responder = new Responder(reqres);
+
+        StateOfChargeWithOneDecimalMessageFilter filter = new StateOfChargeWithOneDecimalMessageFilter();
+        AbstractCommand cmd = new FilteredMonitorCommand(filter);
+
+        Exception caughtException = null;
+        try {
+            cmd.execute(responder.getInput(), responder.getOutput());
+        }
+        catch(Exception e)
+        {
+            caughtException = e;
+        }
+        cmd.doProcessResponse();
+
+        Assert.assertTrue(caughtException instanceof DataErrorException);
+        Assert.assertEquals("", responder.getMessages());
+        Assert.assertEquals(95.1, vals.get(R.string.col_battery_decimal_SOC));
     }
 
-    public void testProcessesFF() {
-        SpeedPreciseMessageFilter filter = new SpeedPreciseMessageFilter();
-        ObdMessageData messageData = new ObdMessageData("4F2 01 FF 00 10 00 00 80 00");
-        filter.doProcessMessage(messageData);
-        Assert.assertEquals(127.5, filter.getSpeedInKmH());
-    }
-
-    public void testProcesses0102() {
-        SpeedPreciseMessageFilter filter = new SpeedPreciseMessageFilter();
-        ObdMessageData messageData = new ObdMessageData("4F2 20 0A 11 28 00 00 00 97"); // This testcase has been confirmed on Henrik's car!
-        filter.doProcessMessage(messageData);
-        Assert.assertEquals(133.0, filter.getSpeedInKmH());
-    }
-
-    public void testProcessesResponseWhenCharging() {
-        SpeedPreciseMessageFilter filter = new SpeedPreciseMessageFilter();
-        ObdMessageData messageData = new ObdMessageData("4F2 01 00");
-        filter.doProcessMessage(messageData);
-        Assert.assertEquals(-0.001, filter.getSpeedInKmH());
-    }
 }
