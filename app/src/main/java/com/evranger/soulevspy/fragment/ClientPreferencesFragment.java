77668f51cd62662e7cf424a393a754ecab63d7f7
langemand <henrik.scheel@spjeldager.dk>
2019-09-25 08:48:16 +0200

    Prepare contributors-page

--- app/src/main/java/com/evranger/soulevspy/fragment/ClientPreferencesFragment.java
+++ app/src/main/java/com/evranger/soulevspy/fragment/ClientPreferencesFragment.java
@@ -85,6 +85,12 @@ public class ClientPreferencesFragment extends PreferenceFragment implements Sha
             return true;
         }
 
+        if (preference.getKey().equals(getString(R.string.key_application_version)))
+        {
+            displayContributors();
+            return true;
+        }
+
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
@@ -245,5 +251,21 @@ public class ClientPreferencesFragment extends PreferenceFragment implements Sha
         ab.setView(mWebview)
                 .setPositiveButton(android.R.string.ok, null)
                 .show();
+    }
+
+    private void displayContributors() {
+// TODO
+//        Context c = getActivity();
+//
+//        // Prepare the view
+//        WebView view = (WebView) LayoutInflater.from(c).inflate(R.layout.dialog_contributors, null);
+//        view.loadUrl("file:///android_asset/contributors.html");
+//
+//        // Show the dialog
+//        AlertDialog.Builder ab = new AlertDialog.Builder(c, R.style.Theme_AppCompat_Light_Dialog_Alert);
+//        ab.setTitle(R.string.pref_about);
+//        ab.setView(view)
+//                .setPositiveButton(android.R.string.ok, null)
+//                .show();
     }
 }
